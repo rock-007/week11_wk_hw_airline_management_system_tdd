32ad04045163db143390afa53711268c50caa63e
rock-007 <umair.ashraf2019@gmail.com>
2021-06-13 01:05:53 +0100

    Flight test added

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,13 +1,29 @@
+import flight.Flight;
 import org.junit.Before;
 import org.junit.Test;
+import person.crew.CabinCrew;
+import person.crew.Pilot;
+import person.crew.RankType;
+
+import java.util.ArrayList;
 
 public class FlightTest {
 
+    Flight flight;
+    Pilot pilot;
+    CabinCrew cabinCrew1;
 
     @Before
 
     public void before() {
-
+        pilot = new Pilot("Umair", RankType.PILOT.getType(), "23423423");
+        cabinCrew1 = new CabinCrew("Asghar", RankType.AIRHOSTES.getType());
+        flight = new Flight(new ArrayList() {
+            {
+                pilot;
+                cabinCrew1;
+            }
+        });
     }
 
     @Test
